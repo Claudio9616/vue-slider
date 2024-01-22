322ecfa9da873e42606396c5fc70cacaf51b7317
Claudio9616 <clares9611@gmail.com>
2024-01-22 15:08:39 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+console.log(Vue)
+const {createApp} = Vue
+const app = createApp({
+    name: 'Carousel',
+    data(){
+        return {
+           
+        }
+    },
+    methods: {
+       
+    }
+})
+app.mount('#root')
