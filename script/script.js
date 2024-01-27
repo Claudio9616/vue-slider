4af59780d5cf0a4875477e94205e2570efc39671
Claudio9616 <clares9611@gmail.com>
2024-01-27 20:00:37 +0100

    add vue-slider completed

--- script/script.js
+++ script/script.js
@@ -1,14 +1,27 @@
-console.log(Vue)
-const {createApp} = Vue
-const app = createApp({
-    name: 'Carousel',
+console.log('Vue ok',Vue);
+
+const { createApp } = Vue;
+
+const app= createApp({
+    name: 'MyCarousel',
     data(){
         return {
-           
+            destinations,
+            currentIndex: 0
         }
     },
     methods: {
-       
+        goToThePrev() {
+            const firstIndex = this.destinations.length -1;
+            if(this.currentIndex === 0) this.currentIndex = firstIndex;
+            else this.currentIndex--;
+        },
+        goToTheNext(){
+            const lastIndex = this.destinations.length -1;
+            if(this.currentIndex === lastIndex) this.currentIndex = 0;
+            else this.currentIndex++;
+        }
     }
-})
-app.mount('#root')
+});
+
+app.mount('#root');
